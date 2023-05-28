a1be79b285032cc4f95bab0bdcda52b7e179a227
Jerome Saltmarsh <jerome.saltmarsh@gmail.com>
2023-05-28 16:00:12 +0800

    0.4.01

--- lib/lemon_engine.dart
+++ lib/lemon_engine.dart
@@ -1,7 +1,4 @@
 library lemon_engine;
 
-export 'src/ease.dart';
 export 'src/engine.dart';
-export 'src/hsv_to_color.dart';
-export 'src/interpolate.dart';
 export 'src/keycode.dart';
--- lib/src/interpolate.dart
+++
@@ -1,7 +0,0 @@
-
-/// @t a decimal between 0 and 1
-int interpolate({required int start, required int end, required double t}) =>
-  (start * (1.0 - t) + end * t).toInt();
-
-double interpolateDouble({required double start, required double end, required double t}) =>
-    (start * (1.0 - t) + end * t);
